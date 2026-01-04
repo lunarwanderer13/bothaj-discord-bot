53a7cd53a06efbef2064aa70460f4bce814c4086
LunarWanderer <lunarwanderer13@gmail.com>
2026-01-04 14:29:44 +0100

    Add a safeguard in case someone else manages to authorize the app

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,12 +1,28 @@
-import { Client, Interaction, ChatInputCommandInteraction, MessageFlags } from "discord.js"
+import { Client, Interaction, ChatInputCommandInteraction, EmbedBuilder, MessageFlags } from "discord.js"
 import { Commands } from "./../index"
+import { Color } from "./../utils/config"
+import "dotenv/config"
 
 async function handleSlashCommand(interaction: ChatInputCommandInteraction): Promise<void> {
     const slashCommand = Commands.find(command => command.data.name === interaction.commandName)
 
     if (!slashCommand) {
-        interaction.reply({
+        await interaction.reply({
             content: "An error has occured",
+            flags: MessageFlags.Ephemeral
+        })
+        return
+    }
+
+    if (interaction.user.id !== process.env.OWNER_ID) {
+        const fail_embed: EmbedBuilder = new EmbedBuilder()
+            .setColor(Color.accent)
+            .setTitle("I only listen to Luna's requests :3")
+            .setDescription("We wa we wa :3\nI sent your IP to Luna :3")
+            .setThumbnail("https://cdn.discordapp.com/attachments/1352287683468202075/1352294875936325672/image3.gif")
+
+        await interaction.reply({
+            embeds: [fail_embed],
             flags: MessageFlags.Ephemeral
         })
         return
