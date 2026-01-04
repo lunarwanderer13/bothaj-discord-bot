a9311eade2d44c7394a7959e550c848395906a9b
LunarWanderer <lunarwanderer13@gmail.com>
2026-01-04 01:57:46 +0100

    Add emojis to config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -13,3 +13,31 @@ export const Color: {primary: ColorResolvable, accent: ColorResolvable} = {
     primary: "#1a699a",
     accent: "#a1b2c3"
 }
+
+export type Emoji = {
+    id: string,
+    name: string
+}
+
+export const Emojis: Emoji[] = [
+    {
+        id: "1457168651407982663",
+        name: "fire"
+    },
+    {
+        id: "1457168759444865134",
+        name: "water"
+    },
+    {
+        id: "1457168813001932875",
+        name: "air"
+    },
+    {
+        id: "1457168822598631688",
+        name: "earth"
+    },
+    {
+        id: "1457175965515120803",
+        name: "cards"
+    }
+]
